1ec9a7083d9e59536b44cae83eb39d63d060cf29
Randall71 <yannbilos@gmail.com>
2020-07-06 13:46:32 +0000

    ajout des fichiers contenant la liste des mots et des lettres

--- src/App.js
+++ src/App.js
@@ -1,27 +1,84 @@
 import React from 'react'
 import './App.css'
+import {Words, letters} from './Words'
 
-const letters = ['A', 'B', 'C', 'D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U','V', 'W', 'X', 'Y', 'Z']
+
 
 class App extends React.Component{
   constructor(props){
     super(props)
     this.state = {
-
+      word: [],
+      renderWord:  ['a','b']
     }
+
+    this.entierAleatoire = this.entierAleatoire.bind(this)
+    this.chooseWord = this.chooseWord.bind(this)
+    this.convertStringToArray = this.convertStringToArray.bind(this)
+    this.checkLettersInWord = this.checkLettersInWord.bind(this)
+  }
+  
+  componentDidMount(){
+   this.chooseWord()
+
+  }
+
+  chooseWord(){
+    const wordsTab = Words.split('\n')
+    const index = this.entierAleatoire(0, wordsTab.length - 1 )
+    let wordChoosen = this.convertStringToArray(wordsTab[index])
+    
+    this.setState((state, props) => ({
+       word: wordChoosen,
+       renderWord: Array(wordsTab[index].length).fill('_ ', 0, wordsTab[index].length)
+      }))
+  }
+
+  convertStringToArray(string){
+    return [...string]
+  }
+
+  checkLettersInWord(letterClick){
+    // const {word} = this.state
+    // const express = '_ '
+    // const newRenderWord = word.map((letter) =>{
+    //  return letterClick === letter ?
+    //   this.setState({renderWord : letter}) : '_ '
+    // })
+
+    // console.log(newRenderWord)
+  }
+
+  entierAleatoire(min, max){
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
   render(){
+    console.log(this.state.word[2])
+    console.log(this.state.word)
+
+    const {renderWord} = this.state
     return(
-      <div className="lettersContainer">
-        {
+     <div className="container">
+        <div className="wordsContainer">
+         {
+           renderWord.map((letter, index) =>(
+            <span className="word" key={index}>
+              {letter}
+            </span>
+    ))
+         }
+        </div>
+        <div className="lettersContainer">
+        {     
           letters.map((letter,index) =>(
-            <div className="letter">
+            <div className="letter" key={index} onClick={() => this.checkLettersInWord(letter)}>
               {letter}
             </div>
           ))
         }
       </div>
+     </div>
     )
   }
 
